8a6250b8ae8b43b757c45d339617aee9f8e99288
khaled-samy <khaled.s.elkhudary@gmail.com>
2022-03-30 15:21:26 +0300

    create add todo modal form

---
+++ src/components/models/AddTodoForm.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+  Button, Modal, ModalHeader, ModalBody, ModalFooter,
+} from 'reactstrap';
+
+function AddTodoForm({
+  handleChange, handleSubmit, toggle, taskName, taskDescription,
+}) {
+  return (
+    <div>
+      <Modal>
+        <ModalHeader>Modal title</ModalHeader>
+        <ModalBody>
+          <form onSubmit={handleSubmit} autoComplete="off">
+            <div className="form-group">
+              <label htmlFor="taskName">
+                Task Name
+
+                <input type="text" onChange={handleChange} name="taskName" id="taskName" value={taskName} required className="form-control" />
+              </label>
+            </div>
+            <br />
+            <div className="form-group">
+              <label htmlFor="taskDescription">
+                Description
+                <textarea rows="5" onChange={handleChange} name="taskDescription" id="taskDescription" value={taskDescription} required className="form-control" />
+              </label>
+            </div>
+          </form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary">Create</Button>
+          <Button color="secondary" onClick={toggle}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+}
+
+export default AddTodoForm;
