405a67e112415ad8895ab75d81e256556835401d
khaled-samy <khaled.s.elkhudary@gmail.com>
2022-03-30 12:57:44 +0300

    intall dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <h1>hello</h1>
     </div>
   );
 }
